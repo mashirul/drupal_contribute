7ddaa58cc3e9b13a5b34e4c35ad15f3e36afbd6b
mashirul <mashirul@valuebound.com>
2018-01-09 01:27:05 +0530

    Added social post module

---
+++ modules/contrib/social_post/src/Plugin/Network/SocialPostNetworkInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\social_post\Plugin\Network;
+
+use Drupal\social_api\Plugin\NetworkInterface;
+
+/**
+ * Defines a Social Post Network Plugin interface.
+ */
+interface SocialPostNetworkInterface extends NetworkInterface {
+
+
+}
